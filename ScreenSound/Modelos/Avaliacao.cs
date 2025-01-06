b5f782e01334848b2768a86f91b130856e86d6d1
Tiago_Mendes_PC-CASA <tiago.mendes.goes@gmail.com>
2025-01-06 17:34:16 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public Avaliacao(int nota)
+    {
+        if (nota <= 0)
+        { 
+            Nota = 0; 
+        }
+        else if(nota >= 10)
+        {
+            Nota = 10;
+        }
+        else
+        {
+            Nota = nota;
+        }
+    }
+
+    public int Nota { get; }
+
+    public static Avaliacao Parse(string text)
+    {
+        return new Avaliacao(int.Parse(text));
+    }
+}
